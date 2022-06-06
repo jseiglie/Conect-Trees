51f674ae510163958150e96bc1b948385398337f
Javier Leyva Seiglie <96433186+jseiglie@users.noreply.github.com>
2022-06-06 17:08:44 +0200

    universal cookie installed

--- client/src/App.js
+++ client/src/App.js
@@ -12,19 +12,18 @@ import EditColab from "./components/EditColab";
 import EditVideo from "./components/EditVideo";
 import axios from "axios";
 import VbHome from "./views/videoblog/VbHome";
-import VbNewsDisplay from "./components/videoblog/VbNewsDisplay"
+import VbNewsDisplay from "./components/videoblog/VbNewsDisplay";
 import VbAdminDashboard from "./components/videoblog/VbAdminDashboard";
 import VbAdminEditNews from "./components/videoblog/VbAdminEditNews";
 import VbAdminAddNews from "./components/videoblog/VbAdminAddNews";
 function App() {
-  
   const [authState, setAuthState] = useState(false);
 
   useEffect(() => {
     if (localStorage.getItem("accessToken")) {
       setAuthState(true);
     }
-  },[]);
+  }, []);
   // checks for valid token
   useEffect(() => {
     axios
@@ -56,11 +55,20 @@ function App() {
             />
             <Route path="/admin/editvideo/:id" element={<EditVideo />} />
             <Route path="/colaboradores/edit/:id" element={<EditColab />} />
-            <Route path="/videoblog/home" element={<VbHome/>}/>
-            <Route path="/videoblog/news/:id" element={<VbNewsDisplay/>}/>
-            <Route path="/videoblog/admin/dashboard" element={<VbAdminDashboard/>}/>
-            <Route path="/videoblog/admin/editnews/:id" element={<VbAdminEditNews/>} />
-            <Route path="/videoblog/admin/addnews" element={<VbAdminAddNews/>} />
+            <Route path="/videoblog/home" element={<VbHome />} />
+            <Route path="/videoblog/news/:id" element={<VbNewsDisplay />} />
+            <Route
+              path="/videoblog/admin/dashboard"
+              element={<VbAdminDashboard />}
+            />
+            <Route
+              path="/videoblog/admin/editnews/:id"
+              element={<VbAdminEditNews />}
+            />
+            <Route
+              path="/videoblog/admin/addnews"
+              element={<VbAdminAddNews />}
+            />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Nosotros } from "../components/nosotros";
 import { Navbar } from "../components/NavBar";
 import "../styles/home.css";
@@ -9,15 +9,29 @@ import Footer from "../components/Footer";
 import Contacto from "../components/Contacto";
 import ConnecNews from "../components/ConnecNews";
 import DigiTraining from "../components/DigiTraining";
+import {Cookies, useCookies} from "react-cookie"
+import CookieConsent from "react-cookie-consent"
 
 const Home = () => {
-  // useEffect(() => {
-  //   axios.get("http://localhost:3001/api").then((res) => {
-  //     console.log(res.data);
-  //   });
-  // }, []);
+//const [cookie, setCookie] = useCookies([])
+useEffect(()=>{
+<CookieConsent
+debug="true"
+  location="bottom"
+  buttonText="Sure man!!"
+  cookieName="myAwesomeCookieName2"
+  style={{ background: "#2B373B" }}
+  buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+  expires={150}
+>
+  This website uses cookies to enhance the user experience.{" "}
+  <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+</CookieConsent>
+})
 
-  // post ej:
+
+
+
 
   return (
     <>
